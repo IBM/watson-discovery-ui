53e9060615bad3a9ad034de900f4875eebd931d3
Rich Hagarty <rich.hagarty@ibm.com>
2018-01-03 17:09:45 -0800

    autoload data files

--- server/index.js
+++ server/index.js
@@ -14,65 +14,92 @@
  * the License.
  */
 
+'use strict';
+
+require('dotenv').config({
+  silent: true
+});
+
 require('isomorphic-fetch');
+const Promise = require('bluebird');
 const queryString = require('query-string');
 const queryBuilder = require('./query-builder');
 const queryTrendBuilder = require('./query-builder-trending');
-const discovery = require('./watson-discovery-service');
+const WatsonDiscoverySetup = require('../lib/watson-discovery-setup');
+const watson = require('watson-developer-cloud');
 
 /**
  * Back end server which handles initializing the Watson Discovery
  * service, and setting up route methods to handle client requests.
  */
 
-/*eslint no-unused-vars: ["error", {"argsIgnorePattern": "response"}]*/
-const WatsonDiscoServer = new Promise((resolve, reject) => {
-  // getInvironments as sanity check to ensure creds are valid
-  discovery.getEnvironments({})
-    .then(response => {
-      // environment and collection ids are always the same for Watson News
-      const environmentId = discovery.environmentId;
-      const collectionId = discovery.collectionId;
-      queryBuilder.setEnvironmentId(environmentId);
-      queryBuilder.setCollectionId(collectionId);
-      queryTrendBuilder.setEnvironmentId(environmentId);
-      queryTrendBuilder.setCollectionId(collectionId);
-    })
-    .then(response => {
-      // this is the inital query to the discovery service
-      console.log('Initial Search Query at start-up');
-      const params = queryBuilder.search({ 
-        natural_language_query: '',
-        count: 5000,
-        passages: false
-      });
-      return new Promise((resolve, reject) => {
-        discovery.query(params)
-          .then(response =>  {
-            resolve(response);
-          })
-          .catch(error => {
-            console.error(error);
-            reject(error);
-          });
-      });
-    })
-    .then(response => {
-      // console.log("GOT DATA!!!! " + util.inspect(response, false, null));
-      resolve(createServer(response));
-    })
-    .catch(error => {
-      // eslint-disable-next-line no-console
-      console.error(error);
-      reject(error);
-    });
+require('metrics-tracker-client').track();
+
+var environment_id;
+var collection_id;
+const DEFAULT_NAME = 'airbnb-austin-data';
+var discoveryDocs = [];
+const fs = require('fs');
+const path = require('path');
+var arrayOfFiles = fs.readdirSync('./data/airbnb/');
+arrayOfFiles.forEach(function(file) {
+  discoveryDocs.push(path.join('./data/airbnb/', file));
+});
+// shorten the list if we are loading - trail version of IBM Cloud 
+// is limited to 256MB application size
+discoveryDocs = discoveryDocs.slice(0,500);
+
+const discovery = watson.discovery({
+  // uname/pwd will be pulled in from VCAP_SERVICES or .env
+  version: 'v1',
+  version_date: '2017-11-07'
+});
+
+// make 'query' a promise function
+discovery.query = Promise.promisify(discovery.query);
+
+const discoverySetup = new WatsonDiscoverySetup(discovery);
+const discoverySetupParams = { 
+  default_name: DEFAULT_NAME, 
+  config_name: 'keyword-extraction'   // instead of 'Default Configuration'
+};
+
+const WatsonDiscoServer = new Promise((resolve) => {
+  discoverySetup.setupDiscovery(discoverySetupParams, (err, data) => {
+    if (err) {
+      discoverySetup.handleSetupError(err);
+    } else {
+      console.log('Dicovery is ready!');
+      // now load data into discovery service collection
+      var collectionParams = data;
+    
+      // set collection creds - at this point the collectionParams
+      // will point to the actual credentials, whether the user
+      // entered them in .env for an existing collection, or if
+      // we had to create them from scratch.
+      environment_id = collectionParams.environment_id;
+      collection_id = collectionParams.collection_id;
+      console.log('environment_id: ' + environment_id);
+      console.log('collection_id: ' + collection_id);
+      queryBuilder.setEnvironmentId(environment_id);
+      queryBuilder.setCollectionId(collection_id);
+      queryTrendBuilder.setEnvironmentId(environment_id);
+      queryTrendBuilder.setCollectionId(collection_id);
+
+      collectionParams.documents = discoveryDocs;
+      console.log('Begin loading ' + discoveryDocs.length + 
+        ' json files into discovery. Please be patient as this can take several minutes.');
+      discoverySetup.loadCollectionFiles(collectionParams);
+      resolve(createServer());
+    }
+  });
 });
 
 /**
  * createServer - create express server and handle requests
  * from client.
  */
-function createServer(results) {
+function createServer() {
   const server = require('./express');
 
   // handles search request from search bar
@@ -143,7 +170,7 @@ function createServer(results) {
     var searchQuery = req.params.searchQuery.replace(/\+/g, ' ');
     const qs = queryString.stringify({ 
       query: searchQuery,
-      count: 5000,
+      count: 2000,
       returnPassages: false,
       queryType: 'natural_language_query'
     });
@@ -190,23 +217,43 @@ function createServer(results) {
   server.get('/*', function(req, res) {
     console.log('In /*');
 
+    // this is the inital query to the discovery service
+    console.log('Initial Search Query at start-up');
+    const params = queryBuilder.search({ 
+      natural_language_query: '',
+      count: 2000,
+      passages: false
+    });
+    return new Promise((resolve, reject) => {
+      discovery.query(params)
+        .then(results =>  {
+
+          // add up totals for the sentiment of reviews
+          var totals = getTotals(results);
+    
+          res.render('index', { data: results, 
+            entities: results,
+            categories: results,
+            concepts: results,
+            keywords: results,
+            numMatches: results.matching_results,
+            numPositive: totals.numPositive,
+            numNeutral: totals.numNeutral,
+            numNegative: totals.numNegative
+          });
+    
+          resolve(results);
+        })
+        .catch(error => {
+          console.error(error);
+          reject(error);
+        });
+    });
+    
     // const util = require('util');
     // console.log('++++++++++++ DISCO RESULTS ++++++++++++++++++++');
     // console.log(util.inspect(results, false, null));
 
-    // add up totals for the sentiment of reviews
-    var totals = getTotals(results);
-
-    res.render('index', { data: results, 
-      entities: results,
-      categories: results,
-      concepts: results,
-      keywords: results,
-      numMatches: results.matching_results,
-      numPositive: totals.numPositive,
-      numNeutral: totals.numNeutral,
-      numNegative: totals.numNegative
-    });
   });
 
   return server;

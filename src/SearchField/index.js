37812a57158fca0af972931cf191bf2c43984b23
Steve Martinelli <stevemart@users.noreply.github.com>
2018-08-11 15:01:14 -0400

    Fix typo
    Closes #131

--- src/SearchField/index.js
+++ src/SearchField/index.js
@@ -83,7 +83,7 @@ export default class SearchField extends React.Component {
       <Grid className='search-field-grid'>
         <Grid.Column width={12} verticalAlign='middle' textAlign='center'>
           <Header as='h1' textAlign='center'>
-            Airbnb Review Data for Ausin, TX
+            Airbnb Review Data for Austin, TX
           </Header>
           <Input
             className='searchinput'
